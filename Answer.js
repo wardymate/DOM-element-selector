0ed7effa05e151b61b5c8e820f57a88ece5f7d44
wardymate <chriswarddev@gmail.com>
2015-06-01 14:25:01 +0100

    first test passed

--- Answer.js
+++ Answer.js
@@ -1,9 +1,12 @@
 var $ = function (selector) {
   var elements = [];
 
+  if (selector==="div") {
+    elements = document.getElementsByTagName("div");
+  }
   ////////////////////
   // Your code here //
   ////////////////////
 
   return elements;
-}
+};
