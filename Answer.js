9f7227c3f38cc8a9089295188b5a6547945f9fe1
wardymate <chriswarddev@gmail.com>
2015-06-02 16:27:05 +0100

    5 of 7 tests passing

--- Answer.js
+++ Answer.js
@@ -1,12 +1,29 @@
 var $ = function (selector) {
   var elements = [];
+  var selectors = [];
 
-  if (selector==="div") {
-    elements = document.getElementsByTagName("div");
+  if(selector.includes('.') || selector.includes('#')) {
+    var string = selector.replace('.',',.').replace('#',',#');
+    selectors = string.split(',').filter(Boolean);
+  } else {
+    elements = document.getElementsByTagName(selector);
+    return elements;
   }
-  ////////////////////
-  // Your code here //
-  ////////////////////
+  // console.log(selectors);
+
+  if (selectors.length === 1) {
+    var propertyName = selectors[0].slice(1);
+    if(selectors[0].includes('#')) {
+      elements.push(document.getElementById(propertyName));
+    } else {
+      elements = document.getElementsByClassName(propertyName);
+    }
+  } else if (selectors.length === 2) {
+    elements = document.getElementsByTagName(selectors[0]);
+  } else {
+    elements = document.getElementsByTagName(selectors[0]);
+    console.log(selector);
+  }
 
   return elements;
 };
